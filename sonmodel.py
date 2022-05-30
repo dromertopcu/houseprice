d8de99feccdf584c968e6a22cd7b2b951540d1d3
ethumana <omertopcu@me.com>
2022-05-30 14:55:06 +0300

    edited

--- sonmodel.py
+++ sonmodel.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import xgboost as xgb
 from sklearn.base import TransformerMixin
 from scipy.sparse import csr_matrix
